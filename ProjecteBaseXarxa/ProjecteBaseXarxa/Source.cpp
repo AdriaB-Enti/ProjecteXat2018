cea4db0919ba48468c525a50dfc3d65f44c72c91
Adrià Biarnés (3B) <adriabiarnesbelso@enti.cat>
2018-02-08 18:07:53 +0100

    Commit inicial amb el codi del projecte
    Del codi de xarxes només hi ha la part de client. Falta escriure la del server

---
+++ ProjecteBaseXarxa/ProjecteBaseXarxa/Source.cpp
@@ -0,0 +1,112 @@
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <string>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//CLIENT -- s'hauria d'ajuntar amb el servidor perque el mateix codi pugui servir per les dues coses
+	//(i el mateix programa et demanarà si vols ser server o client)
+
+	sf::TcpSocket socket;
+	//sf::Socket::Status status = socket.connect("192.168.122.156", 5000, sf::seconds(5.f));
+	sf::Socket::Status status = socket.connect("127.0.0.1", 5000, sf::seconds(5.f));
+
+	if (status != sf::Socket::Done)
+	{
+		cout << "No s'ha conectat correctament";
+	}
+	else {
+		cout << "Conexió amb èxit";
+
+	}
+
+
+	//Tot el codi 
+	//------------------------------------------------------ Codi SFML
+	std::vector<std::string> aMensajes;
+
+	sf::Vector2i screenDimensions(800, 600);
+
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
+
+	sf::Font font;
+	if (!font.loadFromFile("courbd.ttf"))
+	{
+		std::cout << "Can't load the font file" << std::endl;
+	}
+
+	sf::String mensaje = " >";
+
+	sf::Text chattingText(mensaje, font, 14);
+	chattingText.setFillColor(sf::Color(0, 160, 0));
+	chattingText.setStyle(sf::Text::Bold);
+
+
+	sf::Text text(mensaje, font, 14);
+	text.setFillColor(sf::Color(0, 160, 0));
+	text.setStyle(sf::Text::Bold);
+	text.setPosition(0, 560);
+
+	sf::RectangleShape separator(sf::Vector2f(800, 5));
+	separator.setFillColor(sf::Color(200, 200, 200, 255));
+	separator.setPosition(0, 550);
+
+	while (window.isOpen())
+	{
+		sf::Event evento;
+		while (window.pollEvent(evento))
+		{
+			switch (evento.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (evento.key.code == sf::Keyboard::Escape)
+					window.close();
+				else if (evento.key.code == sf::Keyboard::Return)
+				{
+					aMensajes.push_back(mensaje);
+					if (aMensajes.size() > 25)
+					{
+						aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+					}
+					mensaje = ">";
+				}
+				break;
+			case sf::Event::TextEntered:
+				if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
+					mensaje += (char)evento.text.unicode;
+				else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
+					mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
+				break;
+			}
+		}
+		window.draw(separator);
+		for (size_t i = 0; i < aMensajes.size(); i++)
+		{
+			std::string chatting = aMensajes[i];
+			chattingText.setPosition(sf::Vector2f(0, 20 * i));
+			chattingText.setString(chatting);
+			window.draw(chattingText);
+		}
+		std::string mensaje_ = mensaje + "_";
+		text.setString(mensaje_);
+		window.draw(text);
+
+
+		window.display();
+		window.clear();
+	}
+
+	socket.disconnect();
+	//fer un socket.disconnect();
+
+
+	return 0;
+}
