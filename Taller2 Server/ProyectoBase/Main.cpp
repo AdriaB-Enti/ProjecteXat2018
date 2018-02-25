bc7207c3fbc59bf88ca821cbf887b6fe150120f2
adria <adriabiarnesbelso@enti.cat>
2018-02-25 20:46:13 +0100

    Refractoring + Server casi acabat
    He organitzat una mica el codi i fet neteja de codi inutil.
    Ara el server envia un missatge a tothom quan algú es desconecta, però
    ha aparegut un bug i ara a vegades alguns missatges no s'envien fins que
    l'altre client envia un missatge.
    Amb això gairebé estaria fet el Taller2, però s'ha d'arreglar el bug

--- Taller2 Server/ProyectoBase/Main.cpp
+++ Taller2 Server/ProyectoBase/Main.cpp
@@ -2,19 +2,15 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#define MAXPLAYERS 3
+#define MAXPLAYERS 2
 
+void sendToAll(std::vector<sf::TcpSocket*> &sockets, std::string mensaje);	//envia un mensaje a todos los clientes conectados
 
 int main()
 {
-	//TODO: REFRACTOR del codi (deixar d'usar el aMensajes, que s'usava en el client)
 	//Servidor Cliente
 	std::vector<sf::TcpSocket*> sockets;
-	
-	std::string textoAEnviar="";	//em sembla que no el faig servir
-	std::vector<std::string> aMensajes;
 	sf::TcpListener listener;
-
 	listener.listen(50000);
 	for (int playerN = 0; playerN < MAXPLAYERS; playerN++)
 	{
@@ -22,29 +18,14 @@ int main()
 		listener.accept(*newSock);
 		newSock->setBlocking(false);
 		sockets.push_back(newSock);
-		textoAEnviar = "Mensaje desde servidor\n";
 
 		std::string texto = "Conexion con ... " + (newSock->getRemoteAddress()).toString() + ":" + std::to_string(newSock->getRemotePort()) + "\n";
 		std::cout << texto;
 
-		for (std::vector<sf::TcpSocket*>::iterator it = sockets.begin(); it != sockets.end(); ++it)			//Enviamos un mensaje a los demás clientes cuando alguien se conecta
-		{
-			std::string newClient = "Se ha conectado un nuevo usuario. Bienvenido!";
-			size_t confirmedSend;
-			sf::Socket::Status st;
-			do
-			{
-				st = (*it)->send(newClient.c_str(), newClient.length(), confirmedSend);
-				if (st == sf::Socket::Status::Partial)
-				{
-					newClient = newClient.substr(confirmedSend + 1, newClient.length());
-				}
-			} while (st == sf::Socket::Status::Partial);
-		}
+		sendToAll(sockets, "Se ha conectado un nuevo usuario. Bienvenido!"); //Enviamos un mensaje a los demás clientes cuando alguien se conecta
 	}
 	listener.close();
-
-	std::cout << "All users connected. Chat is now online!\n";
+	std::cout << "Todos los usuarions conectados. El chat se ha iniciado!\n";
 
 
 	char buffer[100];
@@ -52,49 +33,49 @@ int main()
 
 	while (true)																							//el server estarà sempre obert
 	{
-		for (int playerN = 0; playerN < MAXPLAYERS; playerN++)												//fem un recieve per cada client -- potser dona problemes si un es desconecta (estarem mirant tots els sockets)
+		std::vector<sf::TcpSocket*>::iterator it = sockets.begin();
+		while (it != sockets.end())			//fem un recieve per cada client -- potser dona problemes si un es desconecta (estarem mirant tots els sockets)
 		{
 			//Receive-------------------------------
-			
-			sf::TcpSocket::Status result = sockets.at(playerN)->receive(buffer, 100, bytesReceived);
+			sf::TcpSocket::Status result = (*it)->receive(buffer, 100, bytesReceived);
 			buffer[bytesReceived] = '\0';
 			if (result == sf::TcpSocket::Status::Done)
 			{
-				aMensajes.push_back(std::string(buffer)); //segurament s'ha de passar el buffer a string
-
-				for (int playerSend = 0; playerSend < MAXPLAYERS; playerSend++)								//per cada recieve, fem un send a cada client
-				{
-					//Send-------------------
-					std::string lastMessage = aMensajes.back();
-					size_t confirmedSend;
-					sf::Socket::Status st;
-					do
-					{
-						st = sockets.at(playerSend)->send(lastMessage.c_str(), lastMessage.length(), confirmedSend);	//vigilar lo del lastMessage
-						if (st == sf::Socket::Status::Partial)
-						{
-							lastMessage = lastMessage.substr(confirmedSend + 1, lastMessage.length());	//length() segur?
-						}
-					} while (st == sf::Socket::Status::Partial);
-					//End Send-------------------
-				}
+				std::string lastMessage = std::string(buffer);
+				sendToAll(sockets, lastMessage);
+				++it;
 			}
 			else if (result == sf::TcpSocket::Status::Disconnected)
 			{
-				//enviar a cada
-				//aMensajes.push_back("The user has disconnected");
+				//enviar a cada client
+				std::cout << "Un cliente se ha desconectado" << std::endl;
+				(*it)->disconnect();
+				delete * it;
+				it = sockets.erase(it);
+				sendToAll(sockets, "Un usuario se ha desconectado");
 			}
-
 			//End Receive-------------------------------
 		}
 	}
 
-	for (int playerN = 0; playerN < MAXPLAYERS; playerN++)	//aquí en teoria no arribarà mai.... 
-	{
-		sockets.at(playerN)->disconnect();
-	}
 	system("pause");
 	
 	return 0;
 
+}
+
+void sendToAll(std::vector<sf::TcpSocket*> &sockets, std::string mensaje) {
+	for (std::vector<sf::TcpSocket*>::iterator it = sockets.begin(); it != sockets.end(); ++it)
+	{
+		size_t confirmedSend;
+		sf::Socket::Status st;
+		do
+		{
+			st = (*it)->send(mensaje.c_str(), mensaje.length(), confirmedSend);
+			if (st == sf::Socket::Status::Partial)
+			{
+				mensaje = mensaje.substr(confirmedSend + 1, mensaje.length());
+			}
+		} while (st == sf::Socket::Status::Partial);
+	}
 }
